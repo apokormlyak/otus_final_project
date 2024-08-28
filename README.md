0209cf7dd24fcd4080c4e0d079cccc73f9a2125a
Alisa Pokormlyak <apokormlyak@yandex.ru>
2024-08-28 22:27:09 +0300

    fix configs

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ t.me/SuperEasyWeatherBot
 Ключ для Telegram-бота можно получить у @BotFather, введя /newbot — команду для создания и регистрации нового бота.
 
 API для запрашивания погоды:  https://openweathermap.org/current
+
+
+## Для запуска локального запуска приложения:
+
+1. Запустить postgres: docker-compose up --build
+2. Запустить бот: python main.py
--- helpers/database/db_config.py
+++ helpers/database/db_config.py
@@ -6,9 +6,8 @@ class Settings(BaseSettings):
     APP_NAME: str = "BotSynoptic"
     DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+psycopg2://admin:password@postgres/weather_db")
     ASYNC_DATABASE_URL: str = os.getenv(
-        "ASYNC_DATABASE_URL", "postgresql+asyncpg://admin:password@postgres:5432/weather_db"
+        "ASYNC_DATABASE_URL", "postgresql+asyncpg://postgres:password@0.0.0.0:5434/postgres"
     )
-
 
 
 settings = Settings()
